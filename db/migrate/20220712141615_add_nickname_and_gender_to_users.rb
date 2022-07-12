51615e1dab22537a1feec2894e6c2b1e001fcf33
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-12 23:19:35 +0900

    null: falseの付与

--- db/migrate/20220712141615_add_nickname_and_gender_to_users.rb
+++ db/migrate/20220712141615_add_nickname_and_gender_to_users.rb
@@ -1,6 +1,6 @@
 class AddNicknameAndGenderToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :nickname, :string
-    add_column :users, :gender, :integer
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :gender, :integer, null: false
   end
 end
