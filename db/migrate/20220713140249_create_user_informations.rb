38c38effe6082f47ad4914392b6352c70683dc72
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-13 23:04:23 +0900

    migrationファイルの修正

--- db/migrate/20220713140249_create_user_informations.rb
+++ db/migrate/20220713140249_create_user_informations.rb
@@ -2,11 +2,11 @@ class CreateUserInformations < ActiveRecord::Migration[6.1]
   def change
     create_table :user_informations do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :family_name
-      t.string :given_name
-      t.string :family_name_kana
-      t.string :given_name_kana
-      t.date :birth_date
+      t.string :family_name, null: false
+      t.string :given_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :given_name_kana, null: false
+      t.date :birth_date, null: false
 
       t.timestamps
     end
