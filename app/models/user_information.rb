fd602a71a1565cba700f29055b20f53c47a645ba
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-16 15:30:50 +0900

    validationの追加

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,3 +1,9 @@
 class UserInformation < ApplicationRecord
   belongs_to :user
+
+  validates :family_name, presence: true
+  validates :given_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :given_name_kana, presence: true
+  validates :birth_date, presence: true
 end
