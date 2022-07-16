69fd133ad63c47b1a577f36e992cbd1cc58ad550
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-16 15:35:11 +0900

    全角カナのvalidation追加

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -2,10 +2,11 @@ class UserInformation < ApplicationRecord
   belongs_to :user
 
   ZENKAKU_REGEXP = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  ZENKAKU_KANA_REGEXP = /\A[ァ-ヶー－]+\z/
 
   validates :family_name, presence: true, format: { with: ZENKAKU_REGEXP, message: "は全角で入力してください" }, allow_blank: true
   validates :given_name, presence: true, format: { with: ZENKAKU_REGEXP, message: "は全角で入力してください" }, allow_blank: true
-  validates :family_name_kana, presence: true
-  validates :given_name_kana, presence: true
+  validates :family_name_kana, presence: true, format: { with: ZENKAKU_KANA_REGEXP, message: "は全角で入力してください" }, allow_blank: true
+  validates :given_name_kana, presence: true, format: { with: ZENKAKU_KANA_REGEXP, message: "は全角で入力してください" }, allow_blank: true
   validates :birth_date, presence: true
 end
