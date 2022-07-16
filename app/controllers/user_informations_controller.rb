d898f5456078702320ae7a645eb1b1e7df8561ab
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-16 16:10:48 +0900

    newアクションの実装

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -2,6 +2,7 @@ class UserInformationsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @user_information = current_user.build_user_information
   end
 
   def create
