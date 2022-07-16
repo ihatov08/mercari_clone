2a0ce42f274c9ca10754f67f67881a63e742d187
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-16 15:34:36 +0900

    全角のvalidation追加

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,8 +1,10 @@
 class UserInformation < ApplicationRecord
   belongs_to :user
 
-  validates :family_name, presence: true
-  validates :given_name, presence: true
+  ZENKAKU_REGEXP = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+
+  validates :family_name, presence: true, format: { with: ZENKAKU_REGEXP, message: "は全角で入力してください" }, allow_blank: true
+  validates :given_name, presence: true, format: { with: ZENKAKU_REGEXP, message: "は全角で入力してください" }, allow_blank: true
   validates :family_name_kana, presence: true
   validates :given_name_kana, presence: true
   validates :birth_date, presence: true
