b423fb71e081c2788dfa34bacddae8d7ab29e812
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 16:34:31 +0900

    migrationファイルの修正

--- db/migrate/20220718073345_create_item_conditions.rb
+++ db/migrate/20220718073345_create_item_conditions.rb
@@ -1,7 +1,7 @@
 class CreateItemConditions < ActiveRecord::Migration[6.1]
   def change
     create_table :item_conditions do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
