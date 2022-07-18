378ab1a5d8e35bfdc9b2ec6c0df661b22972445e
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 17:00:24 +0900

    validationの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,9 @@ class Item < ApplicationRecord
   belongs_to :shipping_payer_type
   belongs_to :prefecture
   belongs_to :shipping_day_type
+
+  validates :images, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
