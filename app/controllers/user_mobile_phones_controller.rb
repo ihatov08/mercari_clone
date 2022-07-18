a3078a8299e171a45a2351c2a36538ee8b77ac6e
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 11:57:56 +0900

    createアクションの実装

--- app/controllers/user_mobile_phones_controller.rb
+++ app/controllers/user_mobile_phones_controller.rb
@@ -6,8 +6,23 @@ class UserMobilePhonesController < ApplicationController
   end
 
   def create
+    @user_mobile_phone = current_user.build_user_mobile_phone(user_mobile_phone_params)
+
+    if @user_mobile_phone.save
+      @user_mobile_phone.send_auth_code_to_sms
+      redirect_to verification_user_mobile_phone_path, notice: "電話番号登録に成功しました"
+    else
+      flash.now.alert = "電話番号登録に失敗しました"
+      render :new
+    end
   end
 
   def verification
+  end
+
+  private
+
+  def user_mobile_phone_params
+    params.require(:user_mobile_phone).permit(:number)
   end
 end
