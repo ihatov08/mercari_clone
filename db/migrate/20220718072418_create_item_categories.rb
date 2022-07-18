b8ad1723ff8ba52eb63700472ce059fa04369709
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 16:25:14 +0900

    migrationファイルの修正

--- db/migrate/20220718072418_create_item_categories.rb
+++ db/migrate/20220718072418_create_item_categories.rb
@@ -1,7 +1,7 @@
 class CreateItemCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :item_categories do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
