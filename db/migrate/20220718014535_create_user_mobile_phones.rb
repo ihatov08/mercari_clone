9306c836947b68b77cd573ca95318faf8c3b1736
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 10:47:38 +0900

    migrationファイルの修正

--- db/migrate/20220718014535_create_user_mobile_phones.rb
+++ db/migrate/20220718014535_create_user_mobile_phones.rb
@@ -2,8 +2,8 @@ class CreateUserMobilePhones < ActiveRecord::Migration[6.1]
   def change
     create_table :user_mobile_phones do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :number
-      t.string :auth_code
+      t.string :number, null: false, index: { unique: true }
+      t.string :auth_code, null: false, index: true
 
       t.timestamps
     end
