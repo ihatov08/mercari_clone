5cec5899d8524e4e45851c52846fc3f11c128994
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 13:44:02 +0900

    migrationファイルの修正

--- db/migrate/20220718043958_add_verified_to_user_mobile_phones.rb
+++ db/migrate/20220718043958_add_verified_to_user_mobile_phones.rb
@@ -1,5 +1,5 @@
 class AddVerifiedToUserMobilePhones < ActiveRecord::Migration[6.1]
   def change
-    add_column :user_mobile_phones, :verified, :boolean
+    add_column :user_mobile_phones, :verified, :boolean, null: false, default: false
   end
 end
