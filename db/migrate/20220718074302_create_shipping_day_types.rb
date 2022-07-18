b9bbcfc532935fdb0135639177b289f996453814
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 16:43:55 +0900

    migrationファイルの修正

--- db/migrate/20220718074302_create_shipping_day_types.rb
+++ db/migrate/20220718074302_create_shipping_day_types.rb
@@ -1,7 +1,7 @@
 class CreateShippingDayTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :shipping_day_types do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
