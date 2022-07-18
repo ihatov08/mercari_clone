71016fc74d3c716c324230828c5d6625b36aeb5c
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 11:51:21 +0900

    インスタンスメソッドの追加

--- app/models/user_mobile_phone.rb
+++ app/models/user_mobile_phone.rb
@@ -7,4 +7,10 @@ class UserMobilePhone < ApplicationRecord
   validates :number, format: { with: MOBILE_PHONE_REGEXP }, allow_blank: true
   validates :number, uniqueness: true
   validates :auth_code, presence: true
+
+  def send_auth_code_to_sms
+    TwilioClient
+      .new
+      .send_auth_code_to_sms(to: number, auth_code: auth_code)
+  end
 end
