7f3df100541391886b615aa8d63f83f592eb77dd
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 11:52:34 +0900

    authcodeの生成

--- app/models/user_mobile_phone.rb
+++ app/models/user_mobile_phone.rb
@@ -8,9 +8,21 @@ class UserMobilePhone < ApplicationRecord
   validates :number, uniqueness: true
   validates :auth_code, presence: true
 
+  before_validation :set_auth_code, if: :new_record?
+
   def send_auth_code_to_sms
     TwilioClient
       .new
       .send_auth_code_to_sms(to: number, auth_code: auth_code)
+  end
+
+  private
+
+  def set_auth_code
+    self.auth_code = generate_auth_code
+  end
+
+  def generate_auth_code
+    sprintf("%.4d", rand(10000))
   end
 end
