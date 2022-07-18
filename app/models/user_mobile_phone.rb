02b7b741f18099bd36634f04c757faa34e04c446
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 11:27:52 +0900

    電話番号のvalidation追加

--- app/models/user_mobile_phone.rb
+++ app/models/user_mobile_phone.rb
@@ -1,7 +1,10 @@
 class UserMobilePhone < ApplicationRecord
   belongs_to :user
 
+  MOBILE_PHONE_REGEXP = /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+
   validates :number, presence: true
+  validates :number, format: { with: MOBILE_PHONE_REGEXP }, allow_blank: true
   validates :number, uniqueness: true
   validates :auth_code, presence: true
 end
