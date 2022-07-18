6a5c95ab3f9a4f049b279a092d5f4760aaffd62a
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 16:29:47 +0900

    migrationファイルの修正

--- db/migrate/20220718072902_create_prefectures.rb
+++ db/migrate/20220718072902_create_prefectures.rb
@@ -1,8 +1,8 @@
 class CreatePrefectures < ActiveRecord::Migration[6.1]
   def change
     create_table :prefectures do |t|
-      t.string :name
-      t.string :name_en
+      t.string :name, null: false, index: { unique: true }
+      t.string :name_en, null: false, index: { unique: true }
 
       t.timestamps
     end
