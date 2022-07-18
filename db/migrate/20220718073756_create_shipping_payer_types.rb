085c901379b39af80c45060cfffbc1b6f18a7e99
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 16:39:26 +0900

    migrationファイルの修正

--- db/migrate/20220718073756_create_shipping_payer_types.rb
+++ db/migrate/20220718073756_create_shipping_payer_types.rb
@@ -1,7 +1,7 @@
 class CreateShippingPayerTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :shipping_payer_types do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
