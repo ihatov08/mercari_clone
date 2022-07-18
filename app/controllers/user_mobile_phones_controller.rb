7a20ac0304bea69d068fecc436fa69b07213f5e6
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 11:33:45 +0900

    newアクションの実装　

--- app/controllers/user_mobile_phones_controller.rb
+++ app/controllers/user_mobile_phones_controller.rb
@@ -2,6 +2,7 @@ class UserMobilePhonesController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @user_mobile_phone = current_user.build_user_mobile_phone
   end
 
   def create
