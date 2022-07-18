8db388ca35ffbaa4c69a22f955b300ae8b03eaa2
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 11:13:52 +0900

    validationの実装

--- app/models/user_mobile_phone.rb
+++ app/models/user_mobile_phone.rb
@@ -1,3 +1,7 @@
 class UserMobilePhone < ApplicationRecord
   belongs_to :user
+
+  validates :number, presence: true
+  validates :number, uniqueness: true
+  validates :auth_code, presence: true
 end
