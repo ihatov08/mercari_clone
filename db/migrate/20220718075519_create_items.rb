9687a3c46d1b111abcabd44abbfd6a96ebabb6d7
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-18 16:56:11 +0900

    migrationファイルの修正

--- db/migrate/20220718075519_create_items.rb
+++ db/migrate/20220718075519_create_items.rb
@@ -2,14 +2,15 @@ class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
       t.references :user, null: false, foreign_key: true
+      t.json :images, null: false, default: {}
       t.references :item_category, null: false, foreign_key: true
       t.references :item_condition, null: false, foreign_key: true
-      t.string :name
-      t.text :description
+      t.string :name, null: false
+      t.text :description, null: false
       t.references :shipping_payer_type, null: false, foreign_key: true
       t.references :prefecture, null: false, foreign_key: true
       t.references :shipping_day_type, null: false, foreign_key: true
-      t.integer :price
+      t.integer :price, null: false
 
       t.timestamps
     end
