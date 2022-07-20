a21f00e12aa5b5e2842564f1e91fd04d8a75fbdd
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-21 08:42:02 +0900

    migrationファイルの修正

--- db/migrate/20220720233846_create_comments.rb
+++ db/migrate/20220720233846_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
-      t.text :body
+      t.text :body, null: false
 
       t.timestamps
     end
