de6d177587e4561fbd5d3e55d03b8e209590675e
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-20 23:01:11 +0900

    migrationファイルの修正

--- db/migrate/20220720140006_create_favorites.rb
+++ db/migrate/20220720140006_create_favorites.rb
@@ -5,6 +5,8 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
       t.references :item, null: false, foreign_key: true
 
       t.timestamps
+
+      t.index %i[user_id item_id], unique: true
     end
   end
 end
