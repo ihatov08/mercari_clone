35967e73c5b01f6dd72313daff4691ed989b5655
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-22 08:50:58 +0900

    newアクションの実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,18 @@
 class ReportsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_comment
 
   def new
+    @report = @comment.reports.build
+    @report.user_id = current_user.id
   end
 
   def create
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:comment_id])
   end
 end
