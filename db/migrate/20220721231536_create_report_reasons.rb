f03b861d9349d84abfdff016d19502517c59f05a
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-22 08:16:51 +0900

    migrationファイルの修正

--- db/migrate/20220721231536_create_report_reasons.rb
+++ db/migrate/20220721231536_create_report_reasons.rb
@@ -1,7 +1,7 @@
 class CreateReportReasons < ActiveRecord::Migration[6.1]
   def change
     create_table :report_reasons do |t|
-      t.string :name
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
