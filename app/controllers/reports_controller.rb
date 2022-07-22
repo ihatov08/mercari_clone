9b561b41cf341978cfb5739afc7e88543e7ad9a1
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-22 09:09:47 +0900

    createアクションの実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,11 +8,24 @@ class ReportsController < ApplicationController
   end
 
   def create
+    @report = @comment.reports.build(report_params)
+    @report.user_id = current_user.id
+
+    if @report.save
+      redirect_to item_path(@report.comment.item), notice: "通報の作成に成功しました"
+    else
+      flash.now.alert = "通報の作成に失敗しました"
+      render :new
+    end
   end
 
   private
 
   def set_comment
     @comment = Comment.find(params[:comment_id])
+  end
+
+  def report_params
+    params.require(:report).permit(:report_reason_id, :body)
   end
 end
