25144db21f0b4998b6f2b77e9a7a1b1b04357778
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 10:13:54 +0900

    不要なviewの削除

---
+++ app/controllers/stripe_payments_controller.rb
@@ -0,0 +1,19 @@
+class StripePaymentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/stripe_payments_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StripePaymentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
