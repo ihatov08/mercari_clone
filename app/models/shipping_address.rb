e7000ac4d3104753858e1945c2895bceadc617c8
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 20:06:00 +0900

    全角のvalidation追加

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,8 +2,12 @@ class ShippingAddress < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
 
+  ZENKAKU_REGEXP = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+
   validates :family_name, presence: true
+  validates :family_name, format: { with: ZENKAKU_REGEXP, "は全角で入力してください" }, allow_blank: true
   validates :given_name, presence: true
+  validates :given_name, format: { with: ZENKAKU_REGEXP, "は全角で入力してください" }, allow_blank: true
   validates :family_name_kana, presence: true
   validates :given_name_kana, presence: true
   validates :postal_code, presence: true
