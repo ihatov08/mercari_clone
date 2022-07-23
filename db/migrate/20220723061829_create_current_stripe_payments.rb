9c51428b75584a8c6e0be00ffe30f0bf2030bd35
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 15:22:23 +0900

    migrationファイルの修正

--- db/migrate/20220723061829_create_current_stripe_payments.rb
+++ db/migrate/20220723061829_create_current_stripe_payments.rb
@@ -1,7 +1,7 @@
 class CreateCurrentStripePayments < ActiveRecord::Migration[6.1]
   def change
     create_table :current_stripe_payments do |t|
-      t.references :stripe_customer, null: false, foreign_key: true
+      t.references :stripe_customer, null: false, foreign_key: true, index: { unique: true }
       t.references :stripe_payment, null: false, foreign_key: true
 
       t.timestamps
