0c775964997064758134efa14cb6ef8324405124
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 12:44:21 +0900

    createアクションの実装

--- app/controllers/stripe_payments_controller.rb
+++ app/controllers/stripe_payments_controller.rb
@@ -14,6 +14,23 @@ class StripePaymentsController < ApplicationController
   end
 
   def create
+    if params[:redirect_status] == "succeeded"
+      setup_intent = Stripe::SetupIntent.retrieve(params[:setup_intent])
+
+      payment_method = Stripe::PaymentMethod.retrieve(setup_intent.payment_method)
+
+      current_user
+        .stripe_customer
+        .stripe_payments
+        .create!(
+          payment_id: setup_intent.payment_method,
+          **payment_method.card.to_h.slice(:brand, :exp_month, :exp_year, :last4)
+          )
+
+      redirect_to item_stripe_payments_path(@item), notice: "カード情報の登録に成功しました"
+    else
+      redirect_to item_stripe_payments_path(@item), alert: "カード情報の登録に失敗しました"
+    end
   end
 
   def edit
