56bbac7eba8764f5aeee93013c93c76480c770cb
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-24 00:01:56 +0900

    updateアクションの実装

--- app/controllers/current_shipping_addresses_controller.rb
+++ app/controllers/current_shipping_addresses_controller.rb
@@ -1,5 +1,18 @@
 class CurrentShippingAddressesController < ApplicationController
   before_action :authenticate_user!
+
   def update
+    @item = Item.find(params[:item_id])
+
+    @current_shipping_address = CurrentShippingAddress.find_or_initialize_by(
+      user_id: current_user.id
+    )
+
+    if @current_shipping_address.update(shipping_address_id: params[:shipping_address_id])
+      redirect_to item_shipping_addresses_path(@item), notice: "配送先住所を更新しました"
+    else
+      flash.now.alert = "配送先住所の更新に失敗しました"
+      render "shipping_addresses/index"
+    end
   end
 end
