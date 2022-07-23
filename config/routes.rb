1268536af29a638eb4e21a89800deb9c896d1ac8
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 20:13:23 +0900

    routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
     resources :stripe_payments, only: %i[index new destroy]
     get "stripe_payments/create" => "stripe_payments#create", as: :get_create_stripe_payments
     resource :current_stripe_payment, only: %i[update]
+    resources :shipping_addresses
   end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
