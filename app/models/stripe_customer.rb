3d2afbe057271d44dbd9c9f29b2e00a6b00a0484
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 09:36:00 +0900

    stripe customerを作成するcallbackの追加

--- app/models/stripe_customer.rb
+++ app/models/stripe_customer.rb
@@ -3,4 +3,14 @@ class StripeCustomer < ApplicationRecord
 
   validates :stripe_customer_id, presence: true
   validates :stripe_customer_id, uniqueness: true
+
+  before_validation :create_stripe_customer, if: :new_record?
+
+  private
+
+  def create_stripe_customer
+    stripe_customer = Stripe::Customer.create(metadata: { user_id: user.id })
+
+    self.customer_id = stripe_customer.id
+  end
 end
