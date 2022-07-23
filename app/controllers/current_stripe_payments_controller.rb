dcce754936d558bd49c52f7046588978843a6aa5
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 15:47:25 +0900

    updateアクションの修正

--- app/controllers/current_stripe_payments_controller.rb
+++ app/controllers/current_stripe_payments_controller.rb
@@ -2,5 +2,16 @@ class CurrentStripePaymentsController < ApplicationController
   before_action authenticate_user!
 
   def update
+    @item = Item.find(params[:item_id])
+    current_stripe_payemnt = CurrentStripePayment.find_or_initialize_by(
+      stripe_customer_id: current_user.stripe_customer.id
+    )
+
+    if current_stripe_payemnt.update(stripe_payment_id: params[:stripe_payment_id])
+      redirect_to new_item_order_path(@item), notice: "カード情報を更新しました"
+    else
+      flash.now.alert = "カード情報の更新に失敗しました"
+      render "items/show"
+    end
   end
 end
