3e1c072d9f861eb8d37758b9a30dc22bdbc46f06
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 22:49:15 +0900

    createアクションの実装

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,6 +1,6 @@
 class ShippingAddressesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: %i[new]
+  before_action :set_item, only: %i[new create]
 
   def index
   end
@@ -10,6 +10,14 @@ class ShippingAddressesController < ApplicationController
   end
 
   def create
+    @shipping_address = current_user.shipping_addresses.build(shipping_address_params)
+
+    if @shipping_address.save
+      redirect_to item_shipping_addresses_path(@item), notice: "配送先住所の登録に成功しました"
+    else
+      flash.now.alert = "配送先住所の登録に失敗しました"
+      render :new
+    end
   end
 
   def edit
@@ -25,5 +33,20 @@ class ShippingAddressesController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(
+      :family_name,
+      :given_name,
+      :family_name_kana,
+      :given_name_kana,
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :line,
+      :building_name,
+      :phone_number
+    )
   end
 end
