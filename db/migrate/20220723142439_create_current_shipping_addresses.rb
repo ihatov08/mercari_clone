653167f6e1c05a92b8d808ddcb0799886141503d
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 23:26:32 +0900

    migrationファイルの修正

--- db/migrate/20220723142439_create_current_shipping_addresses.rb
+++ db/migrate/20220723142439_create_current_shipping_addresses.rb
@@ -3,6 +3,9 @@ class CreateCurrentShippingAddresses < ActiveRecord::Migration[6.1]
     create_table :current_shipping_addresses do |t|
       t.references :user, null: false, foreign_key: true
       t.references :shipping_address, null: false, foreign_key: true
+      t.index %i[user_id shipping_address_id],
+        unique: true,
+        name: "current_shipping_addresses_user_shipping_address_unique"
 
       t.timestamps
     end
