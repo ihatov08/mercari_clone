cdb299c0b5be4524fdb70ee2c0bbefe0d3de9b35
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 20:01:09 +0900

    migrationファイルの修正

--- db/migrate/20220723105809_create_shipping_addresses.rb
+++ db/migrate/20220723105809_create_shipping_addresses.rb
@@ -2,16 +2,16 @@ class CreateShippingAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :shipping_addresses do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :family_name
-      t.string :given_name
-      t.string :family_name_kana
-      t.string :given_name_kana
-      t.string :postal_code
+      t.string :family_name, null: false
+      t.string :given_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :given_name_kana, null: false
+      t.string :postal_code, null: false
       t.references :prefecture, null: false, foreign_key: true
-      t.string :city
-      t.string :line
-      t.string :building_name
-      t.string :phone_number
+      t.string :city, null: false
+      t.string :line, null: false
+      t.string :building_name, null: false
+      t.string :phone_number, null: false
 
       t.timestamps
     end
