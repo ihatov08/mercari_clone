e1fde532aa1d79e0316fc7f7984568927e2e875c
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 09:42:59 +0900

    validationの実装

--- app/models/stripe_payment.rb
+++ app/models/stripe_payment.rb
@@ -1,3 +1,9 @@
 class StripePayment < ApplicationRecord
   belongs_to :stripe_customer
+
+  validates :payment_id, presence: true
+  validates :brand, presence: true
+  validates :exp_month, presence: true
+  validates :exp_year, presence: true
+  validates :last4, presence: true
 end
