bdd766850b7a07a48fcd4d69b831c3e7276537a0
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 20:09:35 +0900

    全角カナのvalidation追加

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,13 +3,16 @@ class ShippingAddress < ApplicationRecord
   belongs_to :prefecture
 
   ZENKAKU_REGEXP = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  ZENKAKU_KANA_REGEXP = /\A[ァ-ヶー－]+\z/
 
   validates :family_name, presence: true
   validates :family_name, format: { with: ZENKAKU_REGEXP, "は全角で入力してください" }, allow_blank: true
   validates :given_name, presence: true
   validates :given_name, format: { with: ZENKAKU_REGEXP, "は全角で入力してください" }, allow_blank: true
   validates :family_name_kana, presence: true
+  validates :family_name_kana, format: { with: ZENKAKU_KANA_REGEXP, message: "は全角で入力してください" }, allow_blank: true
   validates :given_name_kana, presence: true
+  validates :given_name_kana, format: { with: ZENKAKU_KANA_REGEXP, message: "は全角で入力してください" }, allow_blank: true
   validates :postal_code, presence: true
   validates :city, presence: true
   validates :line, presence: true
