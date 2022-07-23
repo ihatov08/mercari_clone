af14ee12553f847c44e2882adc44b6662aeae9f3
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-24 08:18:25 +0900

    set_itemの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
   def new
   end
 
   def create
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
