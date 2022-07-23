0b7a33be00a5d8a908bdad71c5cb3f5d79e017bf
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 09:40:05 +0900

    migrationファイルの修正

--- db/migrate/20220723003840_create_stripe_payments.rb
+++ db/migrate/20220723003840_create_stripe_payments.rb
@@ -2,11 +2,11 @@ class CreateStripePayments < ActiveRecord::Migration[6.1]
   def change
     create_table :stripe_payments do |t|
       t.references :stripe_customer, null: false, foreign_key: true
-      t.string :payment_id
-      t.string :brand
-      t.integer :exp_month
-      t.integer :exp_year
-      t.string :last4
+      t.string :payment_id, null: false
+      t.string :brand, null: false
+      t.integer :exp_month, null: false
+      t.integer :exp_year, null: false
+      t.string :last4, null: false
 
       t.timestamps
     end
