6f7c65c960b1cd06fb40255dbdbec131d5064c9b
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 09:28:23 +0900

    migrationファイルの修正

--- db/migrate/20220723002637_create_stripe_customers.rb
+++ db/migrate/20220723002637_create_stripe_customers.rb
@@ -1,8 +1,8 @@
 class CreateStripeCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :stripe_customers do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :customer_id
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
+      t.string :customer_id, null: false, index: { unique: true }
 
       t.timestamps
     end
