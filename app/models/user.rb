d57257ec21b282b2ab235e3bf795190397bf3130
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 23:28:03 +0900

    associationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   has_one :user_information, dependent: :destroy
   has_one :user_mobile_phone, dependent: :destroy
   has_one :stripe_customer, dependent: :destroy
+  has_one :current_shipping_address, dependent: :destroy
 
   has_many :items, dependent: :destroy
   has_many :favorites, dependent: :destroy
