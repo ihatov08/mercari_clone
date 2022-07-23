cf6ff2dab9fe34badafbb91043ae07fc355b9c21
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-23 20:03:28 +0900

    validationの実装

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,13 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
+
+  validates :family_name, presence: true
+  validates :given_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :given_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :city, presence: true
+  validates :line, presence: true
+  validates :phone_number, presence: true
 end
