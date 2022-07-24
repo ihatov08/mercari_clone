6e770c61ace59102e19120090326b591a44ae730
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-25 08:55:35 +0900

    migrationファイルの修正

--- db/migrate/20220724235452_create_orders.rb
+++ db/migrate/20220724235452_create_orders.rb
@@ -3,18 +3,18 @@ class CreateOrders < ActiveRecord::Migration[6.1]
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
-      t.integer :status
-      t.string :payment_method_id
-      t.string :payer_family_name
-      t.string :payer_given_name
-      t.string :payer_family_name_kana
-      t.string :payer_given_name_kana
-      t.string :payer_postal_code
-      t.string :payer_prefecture_id
-      t.string :payer_city
-      t.string :payer_line
-      t.string :payer_building_name
-      t.string :payer_phone_number
+      t.integer :status, null: false, default: 10
+      t.string :payment_method_id, null: false
+      t.string :payer_family_name, null: false
+      t.string :payer_given_name, null: false
+      t.string :payer_family_name_kana, null: false
+      t.string :payer_given_name_kana, null: false
+      t.string :payer_postal_code, null: false
+      t.string :payer_prefecture_id, null: false
+      t.string :payer_city, null: false
+      t.string :payer_line, null: false
+      t.string :payer_building_name, null: false
+      t.string :payer_phone_number, null: false
 
       t.timestamps
     end
