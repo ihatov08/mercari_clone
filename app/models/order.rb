2cd2bdf146b4ead072cf204b614dd1c2fc1d0796
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-25 08:59:18 +0900

    validationの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :status, presence: true
+  validates :payment_method_id, presence: true
+  validates :payer_family_name, presence: true
+  validates :payer_given_name, presence: true
+  validates :payer_family_name_kana, presence: true
+  validates :payer_given_name_kana, presence: true
+  validates :payer_postal_code, presence: true
+  validates :payer_prefecture_id, presence: true
+  validates :payer_city, presence: true
+  validates :payer_line, presence: true
+  validates :payer_building_name, presence: true
+  validates :payer_phone_number, presence: true
 end
