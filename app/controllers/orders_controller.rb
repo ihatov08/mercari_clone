b690676e23d9273d301bb599ccdc8cb8e7f06177
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-25 23:54:35 +0900

    注文後のリダイレクト先修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
     @order = Order.new(user: current_user, item: @item)
     if @order.pay!
       UserMailer.notify_ordered(@order).deliver
-      redirect_to @item, notice: "購入に成功しました"
+      redirect_to order_path(@order), notice: "購入に成功しました"
     else
       flash.now.alert = "購入に失敗しました"
       render :new
