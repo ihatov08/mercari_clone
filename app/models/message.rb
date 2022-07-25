1be1eed4db288c830eda14feeb0d8f089b632b58
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-26 08:16:48 +0900

    Messageの送信者と受信者を判定するメソッドを追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,16 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  def sender
+    user
+  end
+
+  def recipient
+    if user_id == order.user_id
+      order.item.user
+    else
+      order.user
+    end
+  end
 end
