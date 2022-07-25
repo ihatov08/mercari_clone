6b6fac85a13dcf04d0cb14d9b51182c42a0693bb
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-26 07:31:32 +0900

    createアクションの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,16 @@ class MessagesController < ApplicationController
   before_action :set_order
 
   def create
+    @message = @order.messages.build(message_params)
+    @message.user_id = current_user.id
+
+    if @message.save
+      redirect_to order_path(@order), notice: "メッセージの作成に成功しました"
+    else
+      @item = @order.item
+      flash.now.alert = "メッセージの作成に失敗しました"
+      render "orders/show"
+    end
   end
 
   private
@@ -14,5 +24,9 @@ class MessagesController < ApplicationController
     return if @order.item.user_id == current_user.id
 
     raise ActiveRecord::RecordNotFound
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
