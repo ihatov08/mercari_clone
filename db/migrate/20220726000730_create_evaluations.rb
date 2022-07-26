9ea0d85e55230c114c6ff22fca52fccdd5544c94
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-26 19:42:12 +0900

    migrationファイルの修正

--- db/migrate/20220726000730_create_evaluations.rb
+++ db/migrate/20220726000730_create_evaluations.rb
@@ -3,8 +3,8 @@ class CreateEvaluations < ActiveRecord::Migration[6.1]
     create_table :evaluations do |t|
       t.string :type
       t.references :order, null: false, foreign_key: true
-      t.boolean :good
-      t.text :comment
+      t.boolean :good, null: false, default: true
+      t.text :comment, null: false
 
       t.timestamps
     end
