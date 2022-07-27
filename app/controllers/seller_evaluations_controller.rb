34d5151c3e549ab469a328ead4b38a0d27201e95
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-27 22:15:53 +0900

    createアクションの実装

--- app/controllers/seller_evaluations_controller.rb
+++ app/controllers/seller_evaluations_controller.rb
@@ -3,6 +3,16 @@ class SellerEvaluationsController < ApplicationController
   before_action :set_order
 
   def create
+    @order.build_seller_evaluation(seller_evaluation_params)
+    @order.status = :completed
+
+    if @order.save
+      redirect_to order_path(@order), notice: "評価の作成に成功しました"
+    else
+      @order.restore_status!
+      flash.now.alert = "評価の作成に失敗しました"
+      render "orders/show"
+    end
   end
 
   private
@@ -10,9 +20,16 @@ class SellerEvaluationsController < ApplicationController
   def set_order
     @order = Order.find(params[:order_id])
 
-    return if @order.payer_id == current_user.id
+    return if @order.user_id == current_user.id
     return if @order.item.user_id == current_user.id
 
     raise ActiveRecord::RecordNotFound
+  end
+
+  def seller_evaluation_params
+    params.require(:seller_evaluation).permit(
+      :good,
+      :comment
+      )
   end
 end
