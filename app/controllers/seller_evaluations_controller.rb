1fb04ee84615fe5a6ff2d060d6b63ba3ebc2bfce
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-27 22:12:54 +0900

    set_orderの実装

--- app/controllers/seller_evaluations_controller.rb
+++ app/controllers/seller_evaluations_controller.rb
@@ -1,6 +1,18 @@
 class SellerEvaluationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_order
 
   def create
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:order_id])
+
+    return if @order.payer_id == current_user.id
+    return if @order.item.user_id == current_user.id
+
+    raise ActiveRecord::RecordNotFound
   end
 end
