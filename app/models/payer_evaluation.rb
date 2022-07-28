b0f8b7b44be761e4152ed7172bd4bc0eadbd9376
Tomoya Yoshida <ihatov08@gmail.com>
2022-07-28 22:40:33 +0900

    評価者名を表示できるように評価者を取得するメソッドを追加

--- app/models/payer_evaluation.rb
+++ app/models/payer_evaluation.rb
@@ -1,3 +1,7 @@
 class PayerEvaluation < Evaluation
   validates :received, acceptance: { message: "にチェックを入れてください" }
+
+  def reviewer
+    order.user
+  end
 end
--- app/models/seller_evaluation.rb
+++ app/models/seller_evaluation.rb
@@ -1,2 +1,5 @@
 class SellerEvaluation < Evaluation
+  def reviewer
+    order.item.user
+  end
 end
