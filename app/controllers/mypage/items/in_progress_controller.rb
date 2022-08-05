f2dc3829e3bd99560cef5e06c565e0394fba3e0e
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-05 23:55:38 +0900

    indexアクションの実装

--- app/controllers/mypage/items/in_progress_controller.rb
+++ app/controllers/mypage/items/in_progress_controller.rb
@@ -3,5 +3,10 @@ class Mypage::Items::InProgressController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @items =
+      current_user
+        .items
+        .joins(:order)
+        .merge(Order.not_completed)
   end
 end
