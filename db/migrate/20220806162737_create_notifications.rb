f94b2ee2e6c339649c89daccde1f8831edcd62cc
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-07 01:29:14 +0900

    migrationファイルの修正

--- db/migrate/20220806162737_create_notifications.rb
+++ db/migrate/20220806162737_create_notifications.rb
@@ -1,8 +1,8 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
