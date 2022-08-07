b2192dff2cb99d607783b3e1006beb4118b52497
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-07 23:14:28 +0900

    createアクションの実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,6 +2,10 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+
+    redirect_to user_path(user), notice: "フォローに成功しました"
   end
 
   def destroy
