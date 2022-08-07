15d5d0e1fdcfb6003844d86c1bfbadef0d1eec14
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-08 06:44:30 +0900

    unique indexの追加

--- db/migrate/20220807213919_create_profiles.rb
+++ db/migrate/20220807213919_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[6.1]
   def change
     create_table :profiles do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
       t.string :avatar
       t.text :introduction
 
