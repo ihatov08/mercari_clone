ac131af9dc4e8a76a5515550896cf7908afb3d3a
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-07 23:03:56 +0900

    フォローアソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :reports, dependent: :destroy
   has_many :shipping_addresses, dependent: :destroy
+  has_many :active_relationships,
+    class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   enum gender: {
       unanswered: 0,
