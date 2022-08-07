7d29b54258d593de069003240f2276d73eebd12b
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-07 23:04:34 +0900

    フォロワーアソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,11 @@ class User < ApplicationRecord
     foreign_key: "follower_id",
     dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships,
+    class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   enum gender: {
       unanswered: 0,
