3a49f9696b6c9e85202f2b1b8d5942dc37c52f94
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 00:02:16 +0900

    newアクションの実装

--- app/controllers/mypage/user_bank_accounts_controller.rb
+++ app/controllers/mypage/user_bank_accounts_controller.rb
@@ -2,8 +2,21 @@ class Mypage::UserBankAccountsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @user_bank_account = current_user.user_bank_accounts.build
   end
 
   def create
+  end
+
+  private
+
+  def user_bank_account_params
+    params.require(:user_bank_account).permit(
+      :name,
+      :kind,
+      :branch_name,
+      :account_number,
+      :account_name
+    )
   end
 end
