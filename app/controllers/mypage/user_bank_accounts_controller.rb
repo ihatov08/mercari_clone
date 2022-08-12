9e3c06c6b8c20e7a87d111cd466dc1e84b9c8336
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-12 23:59:08 +0900

    docker-compose run --rm web rails g controller mypage/user_bank_accounts new create

---
+++ app/controllers/mypage/user_bank_accounts_controller.rb
@@ -0,0 +1,7 @@
+class Mypage::UserBankAccountsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/mypage/user_bank_accounts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Mypage::UserBankAccountsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
