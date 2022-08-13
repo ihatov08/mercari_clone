1cb7f0ac07688fa957cbd24bb66916e5c09ec67c
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 13:22:29 +0900

    docker-compose run --rm web rails g controller mypage/user_transfer_histories new create

---
+++ app/controllers/mypage/user_transfer_histories_controller.rb
@@ -0,0 +1,7 @@
+class Mypage::UserTransferHistoriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ test/controllers/mypage/user_transfer_histories_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Mypage::UserTransferHistoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
