d00a7c297081599a14137ab517aad9b4ef6bf395
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 10:42:48 +0900

    migrationファイルの修正

--- db/migrate/20220813014203_create_current_user_bank_accounts.rb
+++ db/migrate/20220813014203_create_current_user_bank_accounts.rb
@@ -5,6 +5,9 @@ class CreateCurrentUserBankAccounts < ActiveRecord::Migration[6.1]
       t.references :user_bank_account, null: false, foreign_key: true
 
       t.timestamps
+
+      t.index %i[user_id user_bank_account_id], unique: true,
+      name: "user_and_user_bank_account_uniq_index"
     end
   end
 end
