982e1e68ade7afb70ae2696ecf4d8b1063342f01
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 13:12:45 +0900

    user_transfer_historyのi18n

--- app/models/user_transfer_history.rb
+++ app/models/user_transfer_history.rb
@@ -7,4 +7,19 @@ class UserTransferHistory < ApplicationRecord
   validates :kind
   validates :bank_account_branch_name
   validates :bank_account_number
+
+  enum bank_account_kind: {
+    saving: 1,
+    checking: 2
+  }
+
+  class << self
+    def bank_account_kinds_i18n
+      I18n.t("enums.user_transfer_history.kind")
+    end
+  end
+
+  def bank_account_kind_i18n
+    self.class.bank_account_kinds_i18n[bank_account_kind.to_sym]
+  end
 end
