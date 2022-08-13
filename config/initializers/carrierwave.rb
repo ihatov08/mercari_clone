3f95142e7ef8c344809f21feb7485da992ac23e8
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 21:54:36 +0900

    carrierwaveのinitializer追加

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+if ENV['ENABLED_FOG']
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region:                ENV['S3_REGION'],
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_public     = false
+  end
+end
