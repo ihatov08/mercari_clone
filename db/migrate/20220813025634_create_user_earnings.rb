33f6b080ccc685b0f422a6c9a48e5f929000701f
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 11:57:32 +0900

    migrationファイルの修正

--- db/migrate/20220813025634_create_user_earnings.rb
+++ db/migrate/20220813025634_create_user_earnings.rb
@@ -2,7 +2,7 @@ class CreateUserEarnings < ActiveRecord::Migration[6.1]
   def change
     create_table :user_earnings do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :price
+      t.integer :price, null: false
 
       t.timestamps
     end
