2504ab2d3b437a8698e51f7f91ebe9031a8458fc
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 13:03:04 +0900

    validationの実装

--- app/models/user_transfer_history.rb
+++ app/models/user_transfer_history.rb
@@ -1,3 +1,10 @@
 class UserTransferHistory < ApplicationRecord
   belongs_to :user
+
+  validates :price
+  validates :bank_name
+  validates :bank_account_name
+  validates :kind
+  validates :bank_account_branch_name
+  validates :bank_account_number
 end
