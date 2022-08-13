5cb2af60bae27fd08246804bf86632970dc5fa44
Tomoya Yoshida <ihatov08@gmail.com>
2022-08-13 13:31:15 +0900

    newアクションの実装

--- app/controllers/mypage/user_transfer_histories_controller.rb
+++ app/controllers/mypage/user_transfer_histories_controller.rb
@@ -2,6 +2,15 @@ class Mypage::UserTransferHistoriesController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    user_bank_account = current_user.current_user_bank_account.user_bank_account
+    @user_transfer_history =
+      current_user.user_transfer_histories.build(
+        bank_name: user_bank_account.name,
+        bank_account_kind: user_bank_account.kind,
+        bank_account_branch_name: user_bank_account.branch_name,
+        bank_account_name: user_bank_account.account_name,
+        bank_account_number: user_bank_account.account_number
+      )
   end
 
   def create
